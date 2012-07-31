e306fe3ed48efbabf84510622bd23297d9887b79
Natalie You <natalie.you@oracle.com>
2012-07-31 14:26:56 -0700

    ch6 exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -92,10 +92,20 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "with a password that's too short" do
-  before { @user.password = @user.password_confirmation = "a" * 5 }
-  it { should be_invalid }
-end
+  	before { @user.password = @user.password_confirmation = "a" * 5 }
+  	it { should be_invalid }
+  end
 
   describe "return value of authenticate method" do
   	before { @user.save }
