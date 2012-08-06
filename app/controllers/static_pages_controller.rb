65f1d48074ce2802b842a03d7c6996e48918b4ac
Natalie You <natalie.you@oracle.com>
2012-08-06 13:11:43 -0700

    add user following

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,5 @@
 class StaticPagesController < ApplicationController
-  def home
+   def home
     if signed_in?
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(page: params[:page])
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -34,10 +34,6 @@ module SessionsHelper
     session[:return_to] = request.fullpath
   end
 
-  def current_user?(user)
-    user == current_user
-  end
-
   def signed_in_user
     unless signed_in?
       store_location
